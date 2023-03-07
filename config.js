2505f1a49db1674d156f57aee70bd8f8d5f527a8
Your Name <you@example.com>
2023-03-08 02:00:18 +0700

    done

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc mừng 8/3';
+const titleCard = 'Tặng người thương';
+const contentCard = 'Chúc Thảo Liều một ngày 8/3 thật nhiều niềm vui, bình an và hạnh phúc. Thanks for everything. ';
+
+const giftImage = 'IMG_4320.JPG';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
+    
